f87b14e814835ac7fc5158f41d82fa230f553d29
dear  Halimeh <nahalimeh@gmail.com>
2025-03-23 14:17:06 +0430

    build custom hook (useFetch)

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch(url);
+      const json = await response.json();
+
+      setIsLoading(false);
+      setData(json);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading };
+};
